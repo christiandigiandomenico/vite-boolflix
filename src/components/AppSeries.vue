14506f5c883bf1c2970621945fa880a7d0858184
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-03-07 16:20:07 +0100

    Aggiunto fix alle bandiere

--- src/components/AppSeries.vue
+++ src/components/AppSeries.vue
@@ -23,7 +23,41 @@ export default {
         },
 
         getFlagSeriesUrl(language) {
-            return `https://flagcdn.com/16x12/${language.toLowerCase()}.png`;
+
+            let flag = this.series.original_language
+
+            switch(this.series.original_language){
+            case "ja":
+                flag = "jp";
+                break;
+            case "en":
+                flag = "us";
+                break;
+            case "ko":
+                flag = "kr";
+                break;
+            case "da":
+                flag = "dk";
+                break;
+            case "zh":
+                flag = "cn";
+                break;
+            case "cs":
+                flag = "cz"
+                break;
+            case "uk":
+                flag = "gb"
+                break;
+            case "hi":
+                flag = "in"
+                break;
+            case "sv":
+                flag = "se"
+                break;
+            default:
+            }
+
+            return `https://flagcdn.com/16x12/${flag}.png`;
         },
 
         starVote() {
